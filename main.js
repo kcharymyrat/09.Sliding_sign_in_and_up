bd6f8f99e16cd8370cafc4061b3e9ccc0458552e
Charymyrat Garryyev <kcharymyrat@gmail.com>
2021-08-11 18:27:33 +0500

    JS event added

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const signUpSwitch = document.querySelector("#sign_up_switch");
-console.log(signUpSwitch)
+// console.log(signUpSwitch)
+const signInSection = document.querySelector("#sign_in");
+// console.log(signInSection);
+
+signInSection.addEventListener('click', (e) => {
+    // console.log(signInSection.style);
+    signInSection.style.flexDirection = "row-reverse";
+})
