20588b6c955cf7a6374b443e313546c7dbd75278
Charymyrat Garryyev <kcharymyrat@gmail.com>
2021-08-13 01:11:15 +0500

    JS hidden toggle finished

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const signUpSwitch = document.querySelector("#sign_up_switch");
-console.log(signUpSwitch)
+// console.log(signUpSwitch)
 const signInSwitch = document.querySelector("#sign_in_switch");
-console.log(signInSwitch)
+// console.log(signInSwitch)
 const signInFillSection = document.querySelector("#sign_in_fill");
-console.log(signInFillSection);
+// console.log(signInFillSection);
 const signUpFillSection = document.querySelector("#sign_up_fill");
-console.log(signUpFillSection);
+// console.log(signUpFillSection);
+
+// element.classList.toggle("mystyle");
+
+const hiddenSectionToggler = (e) => {
+    signInFillSection.classList.toggle("hidden");
+    signUpFillSection.classList.toggle("hidden");
+}
+
+signUpSwitch.addEventListener("click", hiddenSectionToggler);
+signInSwitch.addEventListener("click", hiddenSectionToggler);
