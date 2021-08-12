aa6a7e3869bfa69cd0debb4f30115e6ca42610c4
Charymyrat Garryyev <kcharymyrat@gmail.com>
2021-08-13 00:39:01 +0500

    Remade HTML ids to classes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const signUpSwitch = document.querySelector("#sign_up_switch");
 // console.log(signUpSwitch)
+const signInSwitch = document.querySelector("#sign_in_switch");
+// console.log(signUpSwitch)
 const signInSection = document.querySelector("#sign_in");
 // console.log(signInSection);
 
-signInSection.addEventListener('click', (e) => {
-    // console.log(signInSection.style);
-    signInSection.style.flexDirection = "row-reverse";
-})
+// signInSection.addEventListener('click', (e) => {
+//     // console.log(signInSection.style);
+//     signInSection.style.flexDirection = "row-reverse";
+// })
