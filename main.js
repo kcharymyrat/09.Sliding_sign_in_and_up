9b466fd2e56ce954920e716ab4d1c2e68a624f83
Charymyrat Garryyev <kcharymyrat@gmail.com>
2021-08-17 17:15:58 +0500

    Just some CSS and JS works

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ const signInUpForm = document.querySelector("#forms");
 // console.log(signInUpForm);
 const signInUpMenu = document.querySelector("#sign_menu");
 // console.log(signInUpMenu);
-
-// element.classList.toggle("mystyle");
+const formSignIn = document.querySelector("#sign_in_form")
+const formSignUp = document.querySelector("#sign_up_form")
+const menuSignIn = document.querySelector("#sign_in_menu")
+const menuSignUp = document.querySelector("#sign_up_menu")
 
 const hiddenSectionToggler = (e) => {
-    signInUpForm.classList.toggle("move-right");
-    signInUpMenu.classList.toggle("move-left");
+    // signInUpForm.classList.toggle("move-right");
+    // signInUpMenu.classList.toggle("move-left");
+    formSignUp.classList.toggle("move-left");
+    menuSignIn.classList.toggle("move-right");
 
 
 }
